ba5301d7e4781e55e9573e8abad21bfa7b154076
naegi <gouge.tristan@gmail.com>
2016-12-23 15:42:31 +0100

    don't tarify photos

--- importData/filemanager.py
+++ importData/filemanager.py
@@ -1,4 +1,3 @@
-import tarfile
 from path import path
 from potion_client import Client
 from utils import ensure_dir
@@ -9,15 +8,9 @@ def addFiles(lot):
     lot.pop('csv', None)
 
     f = client.File().save()
-    tar_path = path(f.path) / 'photos.tar.gz'
     ensure_dir(f.path)
 
-    tarify(lot, tar_path)
+    for key, photo in lot.items():
+        photo.path.copy(path(f.path) / 'APN{}{}'.format(key, photo.path.ext))
 
     return f.id
-
-def tarify(lot, dst):
-    tar = tarfile.open(dst, mode="w:gz")
-    for key, photo in lot.items():
-        tar.add(photo.path, 'APN{}{}'.format(key, photo.path.ext))
-    tar.close()
