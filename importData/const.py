56495dc488a2fd112c3dd48d1bc752b0ffe61ad1
Benjamin BERNARD <benjamin.bernard@openpathview.fr>
2017-02-19 15:38:25 +0100

    Adding missing const file

---
+++ importData/const.py
@@ -0,0 +1,16 @@
+
+class Const:
+    default_config = """
+    {
+        "data-dir" : "~/opv/rederbro/{campaign}/",
+        "pi-location": "pi@192.168.42.1:/home/pi/OpenPathView_wip/picturesInfo.csv",
+        "ISO" : "~/opv/iso/goPro.iso",
+        "clean-sd": false,
+        "import": true,
+        "treat": true,
+        "export": false,
+        "dir-manager-file": false,
+        "id-rederbro": 1,
+        "lots-output-dir": "~/opv/lots/{campaign}"
+    }
+    """
