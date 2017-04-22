83262dda76c9b48233f550a78c38eef0545495f4
naegi <gouge.tristan@gmail.com>
2017-04-22 22:37:38 +0200

    update new client to new new client

--- importData/managedb.py
+++ importData/managedb.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 # coding: utf-8
 
-from opv_api_client import RestClient, RessourceEnum
+from opv_api_client import RestClient, ressources
+from opv_api_client.exceptions import RequestAPIException
+from geojson import Point
 
 client = None
 
@@ -15,7 +17,7 @@ def make_client(api_uri='http://localhost:5000'):
 
 
 def make_campaign(id_malette, name, id_rederbro, description):
-    campaign = client.make(RessourceEnum.campaign)
+    campaign = client.make(ressources.Campaign)
     campaign.id_malette = id_malette
     campaign.name = name
     campaign.id_rederbro = id_rederbro
@@ -25,13 +27,11 @@ def make_campaign(id_malette, name, id_rederbro, description):
 
 
 def make_lot(id_malette, campaign, pictures_path, sensors, goprofailed, takenDate, tile=None):
-    lot = client.make(RessourceEnum.lot)
+    lot = client.make(ressources.Lot)
     lot.id_malette = id_malette
-    lot.id_campaign = campaign.id_campaign
-    lot.id_campaign_malette = campaign.id_malette
+    lot.campaign = campaign
     lot.pictures_path = pictures_path
-    lot.id_sensors_malette = sensors.id_malette
-    lot.id_sensors = sensors.id_sensors
+    lot.sensors = sensors
     lot.goprofailed = goprofailed
     lot.takenDate = takenDate.isoformat()
     lot.tile = tile
@@ -39,11 +39,11 @@ def make_lot(id_malette, campaign, pictures_path, sensors, goprofailed, takenDat
     return lot
 
 def make_sensors(id_malette, alt, lng, lat, degrees, minutes):
-    sensors = client.make(RessourceEnum.sensors)
+    sensors = client.make(ressources.Sensors)
     sensors.id_malette = id_malette
-    sensors.alt = alt
-    sensors.lat = lat
-    sensors.lng = lng
+
+    sensors.gps_pos = Point((lat, lng, alt))
+
     sensors.degrees = degrees
     sensors.minutes = minutes
     sensors.create()
@@ -51,30 +51,28 @@ def make_sensors(id_malette, alt, lng, lat, degrees, minutes):
 
 
 def make_cp(id_malette, search_algo_version, nb_cp, stichable, optimized, lot):
-    cp = client.make(RessourceEnum.cp)
+    cp = client.make(ressources.Cp)
     cp.id_malette = id_malette
     cp.search_algo_version = search_algo_version
     cp.nb_cp = nb_cp
     cp.stichable = stichable
     cp.optimized = optimized
-    cp.id_lot = lot.id_lot
-    cp.id_lot_malette = lot.id_malette
+    cp.lot = lot
     cp.create()
     return cp
 
 
 def make_panorama(id_malette, equirectangular_path, cp):
-    panorama = client.make(RessourceEnum.panorama)
+    panorama = client.make(ressources.Panorama)
     panorama.id_malette = id_malette
     panorama.equirectangular_path = equirectangular_path
-    panorama.id_cp = cp.id_cp
-    panorama.id_cp_malette = cp.id_cp_malette
+    panorama.cp = cp
     panorama.create()
     return panorama
 
 
 def make_tile(id_malette, param_location, fallback_path, extension, resolution, max_level, cube_resolution, panorama):
-    tile = client.make(RessourceEnum.tile)
+    tile = client.make(ressources.Tile)
     tile.id_malette = id_malette
     tile.param_location = param_location
     tile.fallback_path = fallback_path
