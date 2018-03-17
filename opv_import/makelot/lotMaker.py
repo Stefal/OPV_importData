357f314a661b2d7c13024b63b819cd0ddbc2604c
Benjamin BERNARD <benjamin.bernard@openpathview.fr>
2018-03-17 11:54:55 +0100

    ImageSet working algorithm

--- opv_import/makelot/lotMaker.py
+++ opv_import/makelot/lotMaker.py
@@ -16,7 +16,6 @@
 # Email: team@openpathview.fr
 # Description: Lot Maker, takes CSV en pictures and manage them to get coherent set of datas.
 
-import copy
 from path import Path
 from typing import List, Iterator, Dict, Tuple, NamedTuple
 from collections import namedtuple
@@ -32,6 +31,11 @@ CAM_DCIM_PATH = "APN{}/DCIM"
 REF_SEARCH_NB_LOT_GENERATED = 30  # default number lot generated for camera set reference search
 REF_SEARCH_MAX_INCOMPLET_CONSECUTIVE_SET = 7  # Maximum number of accepted consecutive incomplete sets during reference search
 REF_SEARCH_MAX_INCOMPLET_SETS = 10  # Maximum total number of incomplete sets during reference search
+
+THRESHOLD_MAX_CONSECUTIVE_INCOMPLETE_SETS = 10
+THRESHOLD_WINDOW_SIZE = 10
+THRESHOLD_WINDOW_MAX_ERRORS = 6
+SUCCESS_WINDOW_SIZE_NEXT_PARTITION_START_SAVING_POINT = 10
 
 # TODO convert all to typed NamedTuple
 SearchedRefImgSet = namedtuple('SearchedReferenceImgSet', ['ref_set', 'first_img_sets', 'img_set_generator'])
@@ -265,38 +269,31 @@ class LotMaker:
 
     def make_gopro_set_new(
             self,
-            threshold_max_consecutive_incomplete_sets,
-            threshold_incomplete_set_window_size,
-            threshold_incomplete_set_max_in_window) -> List[ImageSet]:
+            threshold_max_consecutive_incomplete_sets: int=THRESHOLD_MAX_CONSECUTIVE_INCOMPLETE_SETS,
+            threshold_incomplete_set_window_size: int=THRESHOLD_WINDOW_SIZE,
+            threshold_incomplete_set_max_in_window: int=THRESHOLD_WINDOW_MAX_ERRORS) -> List[ImageSet]:
         """
+        TODO
         """
-        cam_max_indexes = [f.nb_pic() - 1 for f in self.fetchers]
-        current_indexes = [0] * self.nb_cams
-        set_partitions = []
+        gp_sets = []
 
         # partition while indexes aren't all consumed
-        while current_indexes < cam_max_indexes:
-            partition_start = current_indexes
-
-            p = self.generate_cam_partition(
-                partition_start,
+        for p in self.generate_cam_partition(
+                [0] * self.nb_cams,
                 threshold_max_consecutive_incomplete_sets,
                 threshold_incomplete_set_window_size,
-                threshold_incomplete_set_max_in_window)
-            set_partitions.append(p)
-            current_indexes = p.fetcher_next_indexes
+                threshold_incomplete_set_max_in_window):
 
-        # compile partitions
-        gp_sets = []
-        # compile partitions into gp_sets TODO
+            gp_sets.extend(p.images_sets)
+
         return gp_sets
 
     def generate_cam_partition(
             self,
             partition_start,
-            threshold_max_consecutive_incomplete_sets,
-            threshold_incomplete_set_window_size,
-            threshold_incomplete_set_max_in_window) -> CameraSetPartition:
+            threshold_max_consecutive_incomplete_sets: int=THRESHOLD_MAX_CONSECUTIVE_INCOMPLETE_SETS,
+            threshold_incomplete_set_window_size: int=THRESHOLD_WINDOW_SIZE,
+            threshold_incomplete_set_max_in_window: int=THRESHOLD_WINDOW_MAX_ERRORS) -> CameraSetPartition:
         """
         """
         # test/search reference cycle
@@ -305,8 +302,6 @@ class LotMaker:
         break_reason = "NORMAL"   # change it with constant
         fetcher_next_indexes = [0] * self.nb_cams
 
-        # TODO handle window incomplete set count
-
         indexe_gen = indexes_walk(nb_cams=self.nb_cams, cam_start_indexes=partition_start, cam_max_indexes=cam_max_indexes)
         for cam_indexes in indexe_gen:
             print("lm - cam_indexes: ", cam_indexes)
@@ -314,28 +309,34 @@ class LotMaker:
             print('cam_set')
             print(cam_set)
 
-            if cam_indexes == [4, 4, 4, 6, 5, 4]:
-                input()
-
             # check it's not a similar indexes to what we already tested
-            for s in tested_set:
-                if self.is_equiv_ref(s, cam_set):
-                    continue
+            # for s in tested_set:
+            #     if self.is_equiv_ref(s, cam_set):
+            #         continue
 
             # Test cam_set as reference set
             incomplete_consecutive_sets_count = 0
             reject = False
             gp_sets = []
+            gp_set_since_last_save = []
             break_reason = 'NORMAL'
             incomplete_set_count = 0
             complete_set_count = 0
-            img_set_generator = self.cam_set_generator(reference_set=cam_set, start_indexes=list(fetcher_next_indexes))
+            partition_start = list(fetcher_next_indexes)  # updating start
+            img_set_generator = self.cam_set_generator(reference_set=cam_set, start_indexes=list(cam_indexes))
             max_consecutive_incomplete_sets = 0
+
+            error_window = [0] * threshold_incomplete_set_window_size     # 1 when error, 0 when no errors
+            success_window = [0] * SUCCESS_WINDOW_SIZE_NEXT_PARTITION_START_SAVING_POINT  # success window, use to save fetcher_indexes
+            # so that we rollback at the right position
+
+            gen_img_index = 0
+
             try:
                 for gen_img_set_with_fetcher_indexes in img_set_generator:
                     gen_img_set = gen_img_set_with_fetcher_indexes.set
                     print("gen_img_set", gen_img_set)
-                    gp_sets.append(gen_img_set)
+                    gp_set_since_last_save.append(gen_img_set)
                     if gen_img_set.is_complete():
                         complete_set_count += 1
                         max_consecutive_incomplete_sets = max(max_consecutive_incomplete_sets, incomplete_consecutive_sets_count)
@@ -344,25 +345,48 @@ class LotMaker:
                         incomplete_consecutive_sets_count += 1
                         incomplete_set_count += 1
 
+                    error_window[gen_img_index % len(error_window)] = int(gen_img_set.is_complete())
+                    success_window[gen_img_index % len(success_window)] = int(gen_img_set.is_complete())
+
+                    # if sum(error_window) > threshold_incomplete_set_max_in_window:
+                    #     reject = True
+                    #     print("Thresold max consecutive incomplete sets")
+                    #     print("error_window: ", error_window)
+                    #     # import pdb; pdb.set_trace()
+
                     # rejection or stop conditions
                     if (incomplete_consecutive_sets_count > threshold_max_consecutive_incomplete_sets):
                         reject = True
                         print("incomplete_consecutive_sets_count")
                         print(incomplete_consecutive_sets_count)
                         print("exit A")
+
+                        # if cam_indexes[0] > 820:
+                        # import pdb; pdb.set_trace()
+
                         # input()
                         break
 
-                if not reject:
-                    fetcher_next_indexes = gen_img_set_with_fetcher_indexes.fetcher_next_indexes
-                    print("fetcher_next_indexes: ", fetcher_next_indexes)
+                    gen_img_index += 1
+                    print("gen_img_set: ", gen_img_index)
+                    print("reject: ", reject)
+                    print("success_window: ", success_window)
+
+                    if sum(success_window) == len(success_window):
+                        print("---> Saving back point (fetcher_next_indexes)")
+                        gp_sets.extend(gp_set_since_last_save)   # adding set to generated sets
+                        gp_set_since_last_save = []   # clearing set since last save has we just save this point
+                        fetcher_next_indexes = list(gen_img_set_with_fetcher_indexes.fetcher_next_indexes)
+                        print("---> fetcher_next_indexes: ", fetcher_next_indexes)
             except CameraBackInTimeError as backintime_err:
                 break_reason = "BACK IN TIME"
                 print("backintime_err: ", backintime_err)
                 fetcher_next_indexes = backintime_err.indexes  # Next indexes has this indexes weren't used to make an actual set
 
             tested_set.append(cam_set)
-            if not reject:
+            print("fetcher_next_indexes: ", fetcher_next_indexes)
+            print("partition_start: ", partition_start)
+            if fetcher_next_indexes != partition_start:
                 # generator should not suggest already used image, setting start indexes to the end of the partition
                 print("indexe_gen.send")
                 print(fetcher_next_indexes)
@@ -388,69 +412,75 @@ class LotMaker:
 
         return self.rederbrometa.get_metas()
 
-    def find_meta_ref(self, img_sets: List[ImageSet]) -> SearchRefMeta:  # TODO : test it using PNEcrins J1/J2/J3
-        EPSILON = 3
-        metas = self.rederbrometa.get_metas()
-        max_meta_index = len(metas)
-        max_img_set_index = len(img_sets)
+    def match_metas(self, img_sets: List[ImageSet]):
+        """
+        Associate meta
+        """
 
-        # testing different references
-        for meta_cam_indexes in indexes_walk(nb_cams=2, cam_max_indexes=[max_meta_index, max_img_set_index]):
-            meta_start = meta_cam_indexes[0]
-            img_set_start = meta_cam_indexes[1]
-            print("Testing with reference indexes : meta_start: ", meta_start, " | img_set_start: ", img_set_start)
-            print("Start meta set :")
-            print(metas[meta_start])
-            print("Image start set")
-            print(img_sets[img_set_start])
-            print("--")
-
-            lots = []
-
-            last_index_meta = meta_start
-            last_index_img = img_set_start
-            next_index_meta = meta_start + 1
-            next_index_img = img_set_start + 1
-
-            reject = False
-
-            # Generating lot for current reference
-            while not reject and next_index_meta < max_meta_index and next_index_img < max_img_set_index:
-                print("next_index_meta : ", next_index_meta, " | next_index_img : ", next_index_img)
-                last_meta_was_errored = metas[last_index_meta].has_error()
-
-                diff_meta = metas[next_index_meta].timestamp - metas[last_index_meta].timestamp
-                diff_cam = img_sets[next_index_img][0].get_timestamp() - img_sets[last_index_img][0].get_timestamp()
-
-                print("diff_cam - next date : ", dt(img_sets[next_index_img][0].get_timestamp()), " | last date : ", dt(img_sets[last_index_img][0].get_timestamp()))
-
-                print("diff_meta : ", diff_meta, " | diff_cam : ", diff_cam)
-                # input()
-
-                if diff_meta < diff_cam and abs(diff_meta - diff_cam) > EPSILON:  # CSV entries have less difference than img_sets entries
-                    if (
-                        not metas[next_index_meta].has_error() and
-                        not last_meta_was_errored and img_sets[next_index_img].is_complete()
-                    ):
-                        reject = True
-                elif diff_meta > diff_cam and abs(diff_meta - diff_cam) > EPSILON:
-                    reject = True
-
-                if reject:
-                    print("REJECTED")
-                    continue
-
-                if not reject:
-                    lots.append(Lot(meta=metas[next_index_meta], cam_set=img_sets[last_index_img]))
-                    last_index_meta = next_index_meta
-                    last_index_img = next_index_img
-                    next_index_meta += 1
-                    next_index_img += 1
-                    # make association and continue
-                    # create a representation of both data
-
-            if not reject:
-                print("!!!!!!!!!!!!!!!!!!! Got it !!!!!!!!!!!!!")
+    # def find_meta_ref(self, img_sets: List[ImageSet]) -> SearchRefMeta:  # TODO : test it using PNEcrins J1/J2/J3
+    #     EPSILON = 3
+    #     metas = self.rederbrometa.get_metas()
+    #     max_meta_index = len(metas)
+    #     max_img_set_index = len(img_sets)
+    #
+    #     # testing different references
+    #     for meta_cam_indexes in indexes_walk(nb_cams=2, cam_max_indexes=[max_meta_index, max_img_set_index]):
+    #         meta_start = meta_cam_indexes[0]
+    #         img_set_start = meta_cam_indexes[1]
+    #         print("Testing with reference indexes : meta_start: ", meta_start, " | img_set_start: ", img_set_start)
+    #         print("Start meta set :")
+    #         print(metas[meta_start])
+    #         print("Image start set")
+    #         print(img_sets[img_set_start])
+    #         print("--")
+    #
+    #         lots = []
+    #
+    #         last_index_meta = meta_start
+    #         last_index_img = img_set_start
+    #         next_index_meta = meta_start + 1
+    #         next_index_img = img_set_start + 1
+    #
+    #         reject = False
+    #
+    #         # Generating lot for current reference
+    #         while not reject and next_index_meta < max_meta_index and next_index_img < max_img_set_index:
+    #             print("next_index_meta : ", next_index_meta, " | next_index_img : ", next_index_img)
+    #             last_meta_was_errored = metas[last_index_meta].has_error()
+    #
+    #             diff_meta = metas[next_index_meta].timestamp - metas[last_index_meta].timestamp
+    #             diff_cam = img_sets[next_index_img][0].get_timestamp() - img_sets[last_index_img][0].get_timestamp()
+    #
+    #             print("diff_cam - next date : ", dt(img_sets[next_index_img][0].get_timestamp()), " | last date : ", dt(img_sets[last_index_img][0].get_timestamp()))
+    #
+    #             print("diff_meta : ", diff_meta, " | diff_cam : ", diff_cam)
+    #             # input()
+    #
+    #             if diff_meta < diff_cam and abs(diff_meta - diff_cam) > EPSILON:  # CSV entries have less difference than img_sets entries
+    #                 if (
+    #                     not metas[next_index_meta].has_error() and
+    #                     not last_meta_was_errored and img_sets[next_index_img].is_complete()
+    #                 ):
+    #                     reject = True
+    #             elif diff_meta > diff_cam and abs(diff_meta - diff_cam) > EPSILON:
+    #                 reject = True
+    #
+    #             if reject:
+    #                 print("REJECTED")
+    #                 continue
+    #
+    #             if not reject:
+    #                 lots.append(Lot(meta=metas[next_index_meta], cam_set=img_sets[last_index_img]))
+    #                 last_index_meta = next_index_meta
+    #                 last_index_img = next_index_img
+    #                 next_index_meta += 1
+    #                 next_index_img += 1
+    #                 # make association and continue
+    #                 # create a representation of both data
+    #
+    #         if not reject:
+    #             print("!!!!!!!!!!!!!!!!!!! Got it !!!!!!!!!!!!!")
+    #             return lots
 
 class InvalidReferenceSetError(OpvImportError):
     pass
